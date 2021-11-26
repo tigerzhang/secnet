069eb9904310f35baef6d3fd9976839504db5f3e
Zhang Hu <iamzhanghu@gmail.com>
2021-11-26 21:34:40 +0800

    first commit

---
+++ src/main/java/io/yunba/secnet/SecnetApplication.java
@@ -0,0 +1,13 @@
+package io.yunba.secnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecnetApplication.class, args);
+	}
+
+}
